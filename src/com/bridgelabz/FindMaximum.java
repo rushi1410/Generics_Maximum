0865f3fcda0741655beb1c2c3df0d6abdd649e88
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-19 19:00:41 +0530

    Added: Find Maximum Value Of String

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,16 +1,16 @@
 package com.bridgelabz;
 
 public class FindMaximum {
-	public static float maxOfFloat(Float f1, Float f2, Float f3) {
-		float max = f1;
-		if (f2.compareTo(max) > 0)
-			max = f2;
-		if (f3.compareTo(max) > 0)
-			max = f3;
+	public static String maxOfString(String s1, String s2, String s3) {
+		String max = s1;
+		if (s2.compareTo(max) > 0)
+			max = s2;
+		if (s3.compareTo(max) > 0)
+			max = s3;
 		return max;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("The Maximum Value Between The Three Float is: " + maxOfFloat(2.1f,9.8f,4.3f));
+		System.out.println("The Maximum Value Between The Three String is: " + maxOfString("Apple","Peach","Banana"));
 	}
 }
